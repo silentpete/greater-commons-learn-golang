a3fd9eaa5a09ae79e4e9649585263634453481a2
Unknown <peter.gallerani@gmail.com>
2018-03-20 20:18:39 -0600

    NL8 - exercise 3

---
+++ ninja_level_08/exercise_03/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type user struct {
+	First   string
+	Last    string
+	Age     int
+	Sayings []string
+}
+
+func main() {
+	u1 := user{
+		First: "James",
+		Last:  "Bond",
+		Age:   32,
+		Sayings: []string{
+			"Shaken, not stirred",
+			"Youth is no guarantee of innovation",
+			"In his majesty's royal service",
+		},
+	}
+
+	u2 := user{
+		First: "Miss",
+		Last:  "Moneypenny",
+		Age:   27,
+		Sayings: []string{
+			"James, it is soo good to see you",
+			"Would you like me to take care of that for you, James?",
+			"I would really prefer to be a secret agent myself.",
+		},
+	}
+
+	u3 := user{
+		First: "M",
+		Last:  "Hmmmm",
+		Age:   54,
+		Sayings: []string{
+			"Oh, James. You didn't.",
+			"Dear God, what has James done now?",
+			"Can someone please tell me where James Bond is?",
+		},
+	}
+
+	users := []user{u1, u2, u3}
+
+	// fmt.Println(users)
+
+	// your code goes here
+
+	// got this code from Todd, not really getting this yet
+
+	// create new encoder which returns a pointer to an encoder
+	// directly call Encode on the pointer and pass it users, this will print out users to stdout
+	err := json.NewEncoder(os.Stdout).Encode(users)
+	// check the returned error
+	if err != nil {
+		fmt.Println(err)
+	}
+}
