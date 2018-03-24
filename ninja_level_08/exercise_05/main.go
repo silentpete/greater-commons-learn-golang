3b382ac490cf34f297746614f40d8477e075122d
Unknown <peter.gallerani@gmail.com>
2018-03-23 20:52:27 -0600

    NL8 - exercise 4 and 5

---
+++ ninja_level_08/exercise_05/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type user struct {
+	First   string
+	Last    string
+	Age     int
+	Sayings []string
+}
+
+type ByLast []user
+
+func (a ByLast) Len() int           { return len(a) }
+func (a ByLast) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByLast) Less(i, j int) bool { return a[i].Last < a[j].Last }
+
+func main() {
+	u1 := user{
+		First: "James",
+		Last:  "Bond",
+		Age:   32,
+		Sayings: []string{
+			"Shaken, not stirred",
+			"Youth is no guarantee of innovation",
+			"In his majesty's royal service",
+		},
+	}
+
+	u2 := user{
+		First: "Miss",
+		Last:  "Moneypenny",
+		Age:   27,
+		Sayings: []string{
+			"James, it is soo good to see you",
+			"Would you like me to take care of that for you, James?",
+			"I would really prefer to be a secret agent myself.",
+		},
+	}
+
+	u3 := user{
+		First: "M",
+		Last:  "Hmmmm",
+		Age:   54,
+		Sayings: []string{
+			"Oh, James. You didn't.",
+			"Dear God, what has James done now?",
+			"Can someone please tell me where James Bond is?",
+		},
+	}
+
+	users := []user{u1, u2, u3}
+
+	// your code goes here
+	sort.Slice(users, func(i, j int) bool { return users[i].Age < users[j].Age })
+	for _, user := range users {
+		fmt.Println(user.First, user.Last, "is", user.Age, "years old")
+		fmt.Println("and likes to say:")
+		sort.Strings(user.Sayings)
+		for _, v := range user.Sayings {
+			fmt.Println("\t", v)
+		}
+	}
+
+	sort.Sort(ByLast(users))
+	for _, user := range users {
+		fmt.Println(user.First, user.Last, "is", user.Age, "years old")
+		fmt.Println("and likes to say:")
+		for _, v := range user.Sayings {
+			fmt.Println("\t", v)
+		}
+	}
+}
